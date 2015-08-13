584661965ee26033b376fd82a1a8606d1b7a49c0
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-13 15:31:06 -0400

    start working on cli

--- bin/snippet.js
+++ bin/snippet.js
@@ -8,10 +8,43 @@ var DataStore = lazy('data-store');
 var inquirer = lazy('inquirer');
 var Snippets = require('../');
 var snippets = new Snippets();
+var cli = function() {};
 
 if (!snippets.store.has('initialized')) {
   snippetsInit();
 }
+// 'snippet -g --set=templates:~/templates'
+
+// config api: get, set, del, inject, append, prepend, render, write, copy, cache
+var flags = {
+  init: 'Re-initialize `snippet`.',
+  folder: 'folder of snippets?',
+  engine: 'engine preference?',
+  global: 'save things globally',
+  save: '',
+  option: '',
+  expand: 'expand an emmet snippet from the command line',
+  cache: 'cache a directory of snippets. converts files to JSON objects and stores them on the snippet store.',
+  dump: ''
+};
+
+cli.on('help', function () {
+
+});
+
+cli.on('init', function () {
+
+});
+
+cli.on('global', function () {
+
+});
+
+cli.on('expand', function () {
+ // emmet!
+});
+
+
 
 var msg = wrap('Since this is your first time running snippets, do you mind answering a few configuration-related questions, so we can try to give you the best experience?', {width: 60});
 
